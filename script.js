1a9bfc655171364315040610a998a62d51187571
TejasGMahajan <100075661+TejasGMahajan@users.noreply.github.com>
2022-04-24 19:50:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementsByTagName("h1")[0].innerHTML = 10;
+    setTimeout(() => {
+        document.getElementsByTagName("h1")[0].innerHTML = 9;
+        setTimeout(() => {
+            document.getElementsByTagName("h1")[0].innerHTML = 8;
+            setTimeout(() => {
+                document.getElementsByTagName("h1")[0].innerHTML = 7;
+                setTimeout(() => {
+                    document.getElementsByTagName("h1")[0].innerHTML = 6;
+                    setTimeout(() => {
+                        document.getElementsByTagName("h1")[0].innerHTML = 5;
+                        setTimeout(() => {
+                            document.getElementsByTagName("h1")[0].innerHTML = 4;
+                            setTimeout(() => {
+                                document.getElementsByTagName("h1")[0].innerHTML = 3;
+                                setTimeout(() => {
+                                    document.getElementsByTagName("h1")[0].innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.getElementsByTagName("h1")[0].innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.getElementsByTagName("h1")[0].innerHTML = "Happy Independance Day";
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
